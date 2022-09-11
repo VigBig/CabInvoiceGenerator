1ebff49e63a7d2e226c27a7beec0eebd4023bda8
VighneshBilgi <vhbilgi@gmail.com>
2022-09-11 21:18:11 +0530

    Added: Welcome to the Cab Invoice Generator

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class CabInvoiceGenerator {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Cab Invoice Generator:");
+    }
+
+}
